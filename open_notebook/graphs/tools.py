edf839cd1b898538ca8b6a2a14209997dae972e4
LUIS NOVO <lfnovo@gmail.com>
2024-11-01 19:08:33 -0300

    unused graphs

--- open_notebook/graphs/tools.py
+++ open_notebook/graphs/tools.py
@@ -12,15 +12,15 @@ def get_current_timestamp() -> str:
     return datetime.now().strftime("%Y%m%d%H%M%S")
 
 
-@tool
-def doc_query(doc_id: str, question: str):
-    """
-    name: doc_query
-    Use this tool if you need to investigate into a particular document.
-    Another LLM will read the document and answer the question that you might have.
-    Use this when the user question cannot be answered with the content you have in context.
-    """
-    from open_notebook.graphs.doc_query import graph
+# @tool
+# def doc_query(doc_id: str, question: str):
+#     """
+#     name: doc_query
+#     Use this tool if you need to investigate into a particular document.
+#     Another LLM will read the document and answer the question that you might have.
+#     Use this when the user question cannot be answered with the content you have in context.
+#     """
+#     from temp.doc_query import graph
 
-    result = graph.invoke({"doc_id": doc_id, "question": question})
-    return result["answer"]
+#     result = graph.invoke({"doc_id": doc_id, "question": question})
+#     return result["answer"]
